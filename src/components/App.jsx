a58d6744eb036680295f0179116d47bf31999623
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-08-29 00:33:05 +0200

    create section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+import { Component } from "react";
+import { Section } from "./Section/Section";
+import { FeedbackOptions } from "./Section/FeedbackOptions/FeedbackOptions";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => {
+return {
+  [option]: prevState[option] + 1,
+}
+})
+  }
+
+  render() {
+    const options = Object.keys(this.state);
+
+    return (
+  <div>
+    <Section title="Please leave feedback">
+      <FeedbackOptions
+      options={options} 
+      onLeaveFeedback={this.onLeaveFeedback}
+      />
+    </Section>
+  </div>
+    );
+  }  
 };
+
